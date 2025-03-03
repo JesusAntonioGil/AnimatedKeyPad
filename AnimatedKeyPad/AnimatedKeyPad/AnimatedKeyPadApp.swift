3c25aec83f7af30b38fed8f33c6749e53a153f4b
Jesús Antonio Gil <jesus.antoniogil@telefonica.com>
2025-03-03 10:14:58 +0100

    Create Project

---
+++ AnimatedKeyPad/AnimatedKeyPad/AnimatedKeyPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedKeyPadApp.swift
+//  AnimatedKeyPad
+//
+//  Created by Jesus Antonio Gil on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedKeyPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
